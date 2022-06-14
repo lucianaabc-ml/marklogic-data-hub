69ecfcc55d6719d468c9e278b950bd0865b6dbd6
lucianaabc-ml <Luciana.Abecasis@marklogic.com>
2022-06-14 09:32:45 -0300

    changes

--- marklogic-data-hub-central/ui/src/components/error-boundary/ErrorBoundary.tsx
+++ marklogic-data-hub-central/ui/src/components/error-boundary/ErrorBoundary.tsx
@@ -27,7 +27,9 @@ class ErrorBoundary extends React.Component<Props, State> {
 
   render() {
     if (this.state.hasError) {
-      return <NoMatchRedirect message={this.state.errorMessage}/>;
+      return <div>
+          <NoMatchRedirect message={this.state.errorMessage}/>;
+        </div>
     }
 
     return this.props.children;
